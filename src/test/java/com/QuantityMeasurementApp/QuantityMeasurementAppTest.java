320abcc28cc75457ce9501638ab71c9c2b1d01f8
Bhumika Shrivas <bhumikashrivas.work@gmail.com>
2026-02-23 23:14:00 +0530

    [Bhumika Shrivas]: Refactor code

--- src/test/java/com/QuantityMeasurementApp/QuantityMeasurementAppTest.java
+++ src/test/java/com/QuantityMeasurementApp/QuantityMeasurementAppTest.java
@@ -4,71 +4,71 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class QuantityMeasurementAppTest {
+	
+	 @Test
+	 public void testEquality_FeetToFeet_SameValue() {
+		 Length l1 = new Length(1.0, LengthUnit.FEET);
+		 Length l2 = new Length(1.0, LengthUnit.FEET);
 
-    @Test
-    public void testEquality_FeetToFeet_SameValue() {
-        Length l1 = new Length(1.0, LengthUnit.FEET);
-        Length l2 = new Length(1.0, LengthUnit.FEET);
+	        assertTrue(l1.equals(l2));
+	    }
 
-        assertTrue(l1.equals(l2));
-    }
+	    @Test
+	    public void testEquality_InchToInch_SameValue() {
+	        Length l1 = new Length(1.0, LengthUnit.INCHES);
+	        Length l2 = new Length(1.0, LengthUnit.INCHES);
 
-    @Test
-    public void testEquality_InchToInch_SameValue() {
-        Length l1 = new Length(1.0, LengthUnit.INCHES);
-        Length l2 = new Length(1.0, LengthUnit.INCHES);
+	        assertTrue(l1.equals(l2));
+	    }
 
-        assertTrue(l1.equals(l2));
-    }
+	    @Test
+	    public void testEquality_FeetToInch_EquivalentValue() {
+	        Length l1 = new Length(1.0, LengthUnit.FEET);
+	        Length l2 = new Length(12.0, LengthUnit.INCHES);
 
-    @Test
-    public void testEquality_FeetToInch_EquivalentValue() {
-        Length l1 = new Length(1.0, LengthUnit.FEET);
-        Length l2 = new Length(12.0, LengthUnit.INCHES);
+	        assertTrue(l1.equals(l2));
+	    }
 
-        assertTrue(l1.equals(l2));
-    }
+	    @Test
+	    public void testEquality_InchToFeet_EquivalentValue() {
+	        Length l1 = new Length(12.0, LengthUnit.INCHES);
+	        Length l2 = new Length(1.0, LengthUnit.FEET);
 
-    @Test
-    public void testEquality_InchToFeet_EquivalentValue() {
-        Length l1 = new Length(12.0, LengthUnit.INCHES);
-        Length l2 = new Length(1.0, LengthUnit.FEET);
+	        assertTrue(l1.equals(l2));
+	    }
 
-        assertTrue(l1.equals(l2));
-    }
+	    @Test
+	    public void testEquality_FeetToFeet_DifferentValue() {
+	        Length l1 = new Length(1.0, LengthUnit.FEET);
+	        Length l2 = new Length(2.0, LengthUnit.FEET);
 
-    @Test
-    public void testEquality_FeetToFeet_DifferentValue() {
-        Length l1 = new Length(1.0, LengthUnit.FEET);
-        Length l2 = new Length(2.0, LengthUnit.FEET);
+	        assertFalse(l1.equals(l2));
+	    }
 
-        assertFalse(l1.equals(l2));
-    }
+	    @Test
+	    public void testEquality_InchToInch_DifferentValue() {
+	        Length l1 = new Length(1.0, LengthUnit.INCHES);
+	        Length l2 = new Length(2.0, LengthUnit.INCHES);
 
-    @Test
-    public void testEquality_InchToInch_DifferentValue() {
-        Length l1 = new Length(1.0, LengthUnit.INCHES);
-        Length l2 = new Length(2.0, LengthUnit.INCHES);
+	        assertFalse(l1.equals(l2));
+	    }
 
-        assertFalse(l1.equals(l2));
-    }
+	    @Test
+	    public void testEquality_NullComparison() {
+	        Length l1 = new Length(1.0, LengthUnit.FEET);
 
-    @Test
-    public void testEquality_NullComparison() {
-        Length l1 = new Length(1.0, LengthUnit.FEET);
+	        assertFalse(l1.equals(null));
+	    }
 
-        assertFalse(l1.equals(null));
-    }
+	    @Test
+	    public void testEquality_SameReference() {
+	        Length l1 = new Length(1.0, LengthUnit.FEET);
 
-    @Test
-    public void testEquality_SameReference() {
-        Length l1 = new Length(1.0, LengthUnit.FEET);
+	        assertTrue(l1.equals(l1));
+	    }
 
-        assertTrue(l1.equals(l1));
-    }
-
-    @Test
-    public void testInvalidUnit() {
-        assertThrows(IllegalArgumentException.class, () -> {new Length(1.0, null);});
-    }
+	    @Test
+	    public void testInvalidUnit() {
+	        assertThrows(IllegalArgumentException.class, () -> {new Length(1.0, null);});
+	    }
 }
