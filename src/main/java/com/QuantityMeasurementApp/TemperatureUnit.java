213615f3a479423971c425985825f1a2a93a4d10
Bhumika Shrivas <bhumikashrivas.work@gmail.com>
2026-02-24 10:34:21 +0530

    [Bhumika Shrivas]: Add Temperature Unit

--- src/main/java/com/QuantityMeasurementApp/TemperatureUnit.java
+++ src/main/java/com/QuantityMeasurementApp/TemperatureUnit.java
@@ -8,47 +8,37 @@ public enum TemperatureUnit implements IMeasurable {
 
     CELSIUS {
         @Override
-        public double convertToBaseUnit(double value) {
+        public double toBase(double value) {
             return value;
         }
 
         @Override
-        public double convertFromBaseUnit(double baseValue) {
+        public double fromBase(double baseValue) {
             return baseValue;
         }
     },
 
     FAHRENHEIT {
         @Override
-        public double convertToBaseUnit(double value) {
+        public double toBase(double value) {
             return (value - 32) * 5 / 9;
         }
 
         @Override
-        public double convertFromBaseUnit(double baseValue) {
+        public double fromBase(double baseValue) {
             return (baseValue * 9 / 5) + 32;
         }
     },
 
     KELVIN {
         @Override
-        public double convertToBaseUnit(double value) {
+        public double toBase(double value) {
             return value - 273.15;
         }
 
         @Override
-        public double convertFromBaseUnit(double baseValue) {
+        public double fromBase(double baseValue) {
             return baseValue + 273.15;
         }
     };
-
-    @Override
-    public double getConversionFactor() {
-        return 1.0;
-    }
-
-    @Override
-    public String getUnitName() {
-        return this.name();
-    }
 }
